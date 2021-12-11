af609d1c3b9d23fd7119c1a7daa7c8abe1a18bb7
vntutb05 <52446973+vntutb05@users.noreply.github.com>
2021-12-12 03:45:02 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     PORT:3000 || process.env.PORT,
     database:{
-        uri:"mongodb://db:27017/projectNode",
+        //uri:"mongodb://db:27017/projectNode",
+        uri : "mongodb://mongo:okhSnYkPHk7bFUOvmuEn@containers-us-west-6.railway.app:5614"
     },
     globalVariable:(req,res,next)=>{
         res.locals.success_message=req.flash("success"),
